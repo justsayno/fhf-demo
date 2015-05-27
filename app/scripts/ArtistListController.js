bfc10fb31d011ee36052a8ff10532aa7e6cdf6d2
Seth <contact@sethreid.co.nz>
2015-05-27 22:32:35 +1200

    step-5

--- app/scripts/ArtistListController.js
+++ app/scripts/ArtistListController.js
@@ -1,5 +1,15 @@
 angular.module('app').controller("ArtistListController", 
     ['$scope','ArtistService',
-    function($scope,ArtistService){
+    function($scope, ArtistService){
+
         $scope.artists = ArtistService.GetArtists();
+
+        $scope.deleteArtist = function(artist){
+            for(i = 0; i < $scope.artists.length; i++){
+                if($scope.artists[i] === artist){
+                    $scope.artists.splice(i, 1);
+                }
+            }
+        };
+
 }])
